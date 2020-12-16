5fc31e23b8929afcef8efd9383cc44029eb46543
saythename17 <67771055+saythename17@users.noreply.github.com>
2020-12-16 19:34:46 +0900

    rotate the container over time
    and reposition the container at the bottom-right side of the window.

---
+++ x5Transformations2.cpp
@@ -0,0 +1,209 @@
+/*
+		const char* vertexShaderCode = "#version 410 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"layout (location = 1) in vec2 aTexCoord;\n"
+			"out vec2 TexCoord;"
+			"uniform mat4 transform;"
+			"void main(){"
+			"	gl_Position = transform * vec4(aPos, 1.0);"
+			"	TexCoord = vec2(aTexCoord.x, aTexCoord.y); }\0";
+		const char* fragmentShaderCode = "#version 410 core\n"
+			"out vec4 FragColor;\n"
+			"in vec3 myColor;\n"
+			"in vec2 TexCoord;"
+			"uniform sampler2D texture1;"
+			"uniform sampler2D texture2;"
+			"void main() {"
+			"	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x, TexCoord.y)), 0.5f); }\0";
+*/
+
+
+
+
+
+
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "Shader.h"
+#include <iostream>
+#include <cmath>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+void Esc(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+}
+
+void window_size_change(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+
+int main() {
+
+	//initialize and configue
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//glfw window creation
+	GLFWwindow* window = glfwCreateWindow(1600, 1200, "Xion's OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "★Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, window_size_change);
+
+	//glad : load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//shader program
+	Shader myShader = Shader();
+
+	//set up vertex data(and buffer) and configure vertex attributes
+	float vertices[] = {
+		// positions          // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);//bind the VAO(vertex array object) first. 
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);//set vertex buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	//texture coordinate attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+
+
+	unsigned int texture1, texture2;
+	//texture1
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1); //bind that texture to the currently active texture unit.
+	//set the texture wrapping options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	//set the texture filtering options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_set_flip_vertically_on_load(true);// tell stb_image.h to flip loaded texture's on the y-axis.
+
+	int width, height, nChannels;//number of color channel
+	unsigned char* data = stbi_load("bg.jpg", &width, &height, &nChannels, 0);//C:\\Users\\alphacircle\\Downloads\\svt
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		//glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else std::cout << "★Can't find the image file-1" << std::endl;
+	stbi_image_free(data);//free the image memory
+
+	//texture2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("C:\\Users\\alphacircle\\Downloads\\Vroadway_Blue_Large.png", &width, &height, &nChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else std::cout << "★Check your image file name-2" << std::endl;
+	stbi_image_free(data);
+
+
+	myShader.use();//must activate the shader before setting uniforms!
+	glUniform1i(glGetUniformLocation(myShader.ID, "texture1"), 0);//① set manually
+	myShader.setInt("texture2", 1);//② or use shader class
+
+	//render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		//input
+		Esc(window);
+
+		//render
+		glClearColor(.2f, .3f, .3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		
+		
+		//////////////////////////////////////////////////////////
+		//To rotate the container over time we have to update the transformation matrix in the rendre loop.
+		//we have to re-create the transformation matrix in each iteration of the render loop/ to continuously update the rotation.
+		glm::mat4 transform = glm::mat4(1.0f);//make sure in initialize matrix to identity matrix
+
+		//first rotate the container aroudn the origin (0,0,0).
+		//second translate its rotated version to the bottom-right corner of the screen.
+		//★☆★☆REMEMEBER★☆★☆
+		//the actual transformation order sould be read in reverse 0
+		//	: even though in code we first translate and then later rotate, the actual transformations == rotation->translation. 
+		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		//get matrix's uniform location and set matrix
+		myShader.use();
+		unsigned int transfomLocation = glGetUniformLocation(myShader.ID, "transform");
+		glUniformMatrix4fv(transfomLocation, 1, GL_FALSE, glm::value_ptr(transform));
+		////////////////////////////////////////////////////////////
+
+
+
+
+
+		//render container
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+	return 0;
+}
