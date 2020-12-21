7cea053fc34ed158699612ff22147a3d93110778
saythename17 <67771055+saythename17@users.noreply.github.com>
2020-12-21 20:44:36 +0900

    Create x7Camera..h

---
+++ x7Camera..h
@@ -0,0 +1,106 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader() {
+		const char* vertexShaderCode = "#version 410 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"layout (location = 1) in vec2 aTexCoord;\n"
+			"out vec2 TexCoord;\n"
+			"uniform mat4 model;"
+			"uniform mat4 view;"
+			"uniform mat4 projection;"
+			"void main(){"
+			//clip V = projection M · view M · model M · object V
+			/*Remever that the order of matrix multiplication is reversed(we need to read matrix multiplication from right to left. ←)*/
+			"	gl_Position = projection * view * model * vec4(aPos, 1.0);"
+			"	TexCoord = vec2(aTexCoord.x, aTexCoord.y);}\0"; 
+		const char* fragmentShaderCode = "#version 410 core\n"
+			"out vec4 FragColor;\n"
+			"in vec2 TexCoord;"
+			"uniform sampler2D texture1;"
+			"uniform sampler2D texture2;"
+			"void main() {"
+			"	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.6); }\0";
+		//☆2. compile shaders
+		unsigned int vertexShader, fragmentShader;
+
+		//vertex Shader
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+		glCompileShader(vertexShader);
+		checkCompileError(vertexShader, "VERTEX");
+		//fragment Shader
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+		glCompileShader(fragmentShader);
+		checkCompileError(fragmentShader, "FRAGMENT");
+
+		//shader Program 
+		ID = glCreateProgram();
+		glAttachShader(ID, vertexShader);
+		glAttachShader(ID, fragmentShader);
+		glLinkProgram(ID);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+	}
+
+	//use/activate the shader
+	void use()//activates the shader program
+	{
+		glUseProgram(ID);
+	}
+
+	void setBool(const std::string& name, bool value) const {
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+	}
+	void setInt(const std::string& name, int value) const {
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	}
+	void setFloat(const std::string& name, float value) const {
+		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
+	void setMat4(const std::string& name, const glm::mat4& mat) const {
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+
+private://check shader compile/link errors
+	void checkCompileError(GLuint shader, std::string type) {
+		GLint success;
+		GLchar infoLog[1024];
+		if (type != "PROGRAM") {
+			//print compile errors if any
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success) {
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "★SHADER_COMPILATION_ERROR: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+			else {
+				glGetProgramiv(shader, GL_LINK_STATUS, &success);
+				if (!success) {
+					glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+					std::cout << "★PROGRAM_LINKING_ERROR: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+				}
+			}
+		}
+
+	}
+};
+#endif // !SHADER_H
+
+
+
+
